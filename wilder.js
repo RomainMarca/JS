3942d3891a8f0c96497d829f82e08be9ba0900b0
Romain MARCA <romain.marca@sii.fr>
2019-04-29 17:15:33 +0200

    my solution

---
+++ wilder.js
@@ -0,0 +1,10 @@
+/* hello-wilder.js */
+
+// Define some variables
+const movieName = 'H2G2';
+const year = 2005;
+const director = 'Garth Jennings'
+const message = 'Hello, My favorite movie is ' + movieName + '! The director is ' + director + ' and the release date is' + year+ '.';
+
+// Display a popup
+alert(message);
